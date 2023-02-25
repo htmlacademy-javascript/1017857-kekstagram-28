9dc0dbf8c0e9a2cb3b860b421b22afea9fee3f45
niknov80 <n.novozhilov80@gmail.com>
2023-02-25 16:27:11 +0300

    Проведен рефакторинг кода, добавлены описания функций в формате JSDoc, генерация моковых данных вынесена в отднльную папку

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import './functions.js';
-import {createPhotos} from './data.js';
+import {generateUserPhotoData} from './mocks/data.js';
 
-console.log(createPhotos());
+const COUNT_OF_PHOTOS = 25;
+export const userPhotoData = generateUserPhotoData(COUNT_OF_PHOTOS);
