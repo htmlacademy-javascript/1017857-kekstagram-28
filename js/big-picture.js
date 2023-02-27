11b592783b76a6f950f8c486aeba153dbdc62716
niknov80 <n.novozhilov80@gmail.com>
2023-02-27 21:58:08 +0300

    module8-task2 Добавлен вывод комментариев по нажатию кнопки

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,27 +1,9 @@
 import {isEscapeKey} from './util.js';
+import {modifyCommentList} from './comments.js';
 
 const bodyElement = document.querySelector('body');
 const bigPictureElement = document.querySelector('.big-picture');
 const closeBigPictureElement = bigPictureElement.querySelector('#picture-cancel');
-const commentListElement = bigPictureElement.querySelector('.social__comments');
-const commentItemTemplate = commentListElement.querySelector('.social__comment');
-const commentListFragment = document.createDocumentFragment();
-
-/**
- * Функция выводит список комментариев пользователей
- * @param {Array} commentsData - Список комменатриев к фотографии
- */
-const modifyCommentList = (commentsData) => {
-  commentListElement.innerHTML = '';
-  commentsData.forEach((commentData) => {
-    const commentItemElement = commentItemTemplate.cloneNode(true);
-    commentItemElement.querySelector('.social__picture').setAttribute('src', commentData.avatar);
-    commentItemElement.querySelector('.social__picture').setAttribute('alt', commentData.name);
-    commentItemElement.querySelector('.social__text').textContent = commentData.message;
-    commentListFragment.append(commentItemElement);
-  });
-  commentListElement.append(commentListFragment);
-};
 
 /**
  * Функция выводит большое изображение на основе полученных данных
@@ -31,8 +13,6 @@ const modifyBigPicture = (photoData) => {
   bigPictureElement.querySelector('.big-picture__img img').setAttribute('src', photoData.url);
   bigPictureElement.querySelector('.likes-count').textContent = photoData.likes;
   bigPictureElement.querySelector('.social__caption').textContent = photoData.description;
-  bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');
-  bigPictureElement.querySelector('.comments-loader').classList.add('hidden');
   modifyCommentList(photoData.comments);
 };
 
---
+++ js/comments.js
@@ -0,0 +1,38 @@
+const social = document.querySelector('.social');
+const commentListElement = social.querySelector('.social__comments');
+const commentItemTemplate = commentListElement.querySelector('.social__comment');
+const commentListFragment = document.createDocumentFragment();
+const COMMENT_COUNT_INIT = 5;
+
+let commentsCount = COMMENT_COUNT_INIT;
+const addComments = (commentsData, count) => {
+  commentListElement.innerHTML = '';
+  for (let i = 0; i < count; i ++) {
+    if (commentsData.length === i) {
+      social.querySelector('.comments-loader').classList.add('hidden');
+      break;
+    }
+    const commentItemElement = commentItemTemplate.cloneNode(true);
+    commentItemElement.querySelector('.social__picture').setAttribute('src', commentsData[i].avatar);
+    commentItemElement.querySelector('.social__picture').setAttribute('alt', commentsData[i].name);
+    commentItemElement.querySelector('.social__text').textContent = commentsData[i].message;
+    commentListFragment.append(commentItemElement);
+  }
+  commentListElement.append(commentListFragment);
+};
+
+/**
+ * Функция выводит список комментариев пользователей
+ * @param {Array} commentsData - Список комментариев к фотографии
+ */
+const modifyCommentList = (commentsData) => {
+  social.querySelector('.comments-count').textContent = commentsData.length;
+  addComments(commentsData, commentsCount);
+  social.querySelector('.comments-loader').addEventListener('click', () => {
+    commentsCount += COMMENT_COUNT_INIT;
+    addComments(commentsData, commentsCount);
+  });
+};
+
+
+export {modifyCommentList};
