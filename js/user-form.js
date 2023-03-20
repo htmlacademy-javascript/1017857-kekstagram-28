620b4431acd934ab9d51d2468e019bfe8651738c
niknov80 <n.novozhilov80@gmail.com>
2023-03-20 21:25:31 +0300

    FEAT Выбранная пользователем фотография загружаться в поле загрузки файлов в форме загрузки и показываться в окне.

--- js/user-form.js
+++ js/user-form.js
@@ -2,12 +2,24 @@ import {setScaleClick} from './scale.js';
 import {setEffectLevelSliderClickUpdate} from './effect.js';
 import {setUserFormSubmit} from './user-form-validation.js';
 
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const fileChooser = document.querySelector('#upload-file');
 const userPicture = document.querySelector('.img-upload__preview img');
 
 const setUserForm = () => {
   setScaleClick(userPicture);
   setEffectLevelSliderClickUpdate(userPicture);
   setUserFormSubmit(userPicture);
+
+  fileChooser.addEventListener('change', () => {
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+    if (matches) {
+      userPicture.src = URL.createObjectURL(file);
+    }
+  });
 };
 
 export {setUserForm};
