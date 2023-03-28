69461f82fbd530619fb205a48abfd6b09be7016f
niknov80 <n.novozhilov80@gmail.com>
2023-03-28 21:17:45 +0300

    REF Рефакторинг модуля effect.js

--- js/effect.js
+++ js/effect.js
@@ -1,64 +1,97 @@
-const VALUE_SLIDER_INIT = 100;
+const effects = {
+  none: {
+    name: 'none',
+    filter: '',
+    range: {
+      min: 0,
+      max: 100
+    },
+    start: 100,
+    step: 1,
+    connect: 'lower'
+  },
+  chrome: {
+    name: 'chrome',
+    filter: 'grayscale',
+    units: '',
+    range: {
+      min: 0,
+      max: 1
+    },
+    start: 1,
+    step: 0.1
+  },
+  sepia: {
+    name: 'sepia',
+    filter: 'sepia',
+    units: '',
+    range: {
+      min: 0,
+      max: 1
+    },
+    start: 1,
+    step: 0.1
+  },
+  marvin: {
+    name: 'marvin',
+    filter: 'invert',
+    units: '%',
+    range: {
+      min: 1,
+      max: 100
+    },
+    start: 100,
+    step: 1
+  },
+  phobos: {
+    name: 'phobos',
+    filter: 'blur',
+    units: 'px',
+    range: {
+      min: 0,
+      max: 3
+    },
+    start: 3,
+    step: 0.1
+  },
+  heat: {
+    name: 'heat',
+    filter: 'brightness',
+    units: '',
+    range: {
+      min: 0,
+      max: 3
+    },
+    start: 3,
+    step: 0.1
+  },
+};
 
 const effectLevelSliderElement = document.querySelector('.effect-level__slider');
 const effectLevelElement = document.querySelector('.effect-level__value');
 const effectSliderContainer = document.querySelector('.effect-level');
-
-/**
- * Функция скрывает слайдер выбора диапазона значений эффекта
- */
-const hideSlider = () => {
-  effectSliderContainer.classList.add('hidden');
-};
-
-/**
- * Функция показывает слайдер выбора диапазона значений эффекта
- */
-const showSlider = () => {
-  effectSliderContainer.classList.remove('hidden');
-};
+const effectList = document.querySelector('.effects__list');
+let currentFilter = effects.none.name;
 
 noUiSlider.create(effectLevelSliderElement, {
   range: {
-    min: 0,
-    max: 100,
+    min: effects.none.range.min,
+    max: effects.none.range.max,
   },
-  start: 80,
-  step: 1,
-  connect: 'lower',
+  start: effects.none.start,
+  step: effects.none.step,
+  connect: effects.none.connect,
 });
 
 /**
- * Функция для получения названия эффекта
+ * Функция для устанавливает стиль эффекта
  * @param {Node} userPicture - пользовательское изображение. HTML элемент.
  * @param {string} filterValue - название эффекта
  */
-const getFilter = (userPicture, filterValue) => {
-  const effect = userPicture.classList.value.split('--')[1];
-  let filter;
-  switch (effect) {
-    case 'chrome':
-      filter = `grayscale(${filterValue})`;
-      break;
-    case 'sepia':
-      filter = `sepia(${filterValue})`;
-      break;
-    case 'marvin':
-      filter = `invert(${filterValue}%)`;
-      break;
-    case 'phobos':
-      filter = `blur(${filterValue}px)`;
-      break;
-    case 'heat':
-      filter = `brightness(${filterValue})`;
-      break;
-    default:
-      filter = '';
-  }
-  userPicture.style.filter = filter;
+const setFilter = (userPicture, filterValue) => {
+  const filterUnit = effects[currentFilter].units ? effects[currentFilter].units : '';
+  userPicture.style.filter = `${effects[currentFilter].filter}(${filterValue}${filterUnit})`;
 };
-
-const effectList = document.querySelector('.effects__list');
 
 /**
  * Функция сброса эффекта в исходное состояние - нейтральный фильтр со значением 100
@@ -67,8 +100,8 @@ const effectList = document.querySelector('.effects__list');
 const resetEffect = (userPicture) => {
   userPicture.className = '';
   userPicture.classList.add('effects__preview--none');
-  hideSlider();
-  effectLevelSliderElement.noUiSlider.set(VALUE_SLIDER_INIT);
+  effectSliderContainer.classList.add('hidden');
+  effectLevelSliderElement.noUiSlider.set(effects.none.start);
 };
 
 /**
@@ -78,73 +111,26 @@ const resetEffect = (userPicture) => {
 const addEffectLevelSliderClickUpdateHandler = (userPicture) => {
   effectLevelSliderElement.noUiSlider.on('update', () => {
     effectLevelElement.value = effectLevelSliderElement.noUiSlider.get();
-    getFilter(userPicture, effectLevelElement.value);
+    setFilter(userPicture, effectLevelElement.value);
   });
 
   effectList.addEventListener('click', (evt) => {
     userPicture.className = '';
-    userPicture.classList.add(`effects__preview--${evt.target.value}`);
-    switch (evt.target.value) {
-      case 'chrome':
-        showSlider();
-        effectLevelSliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1
-          },
-          start: 1,
-          step: 0.1
-        });
-        break;
-      case 'sepia':
-        showSlider();
-        effectLevelSliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1
-          },
-          start: 1,
-          step: 0.1
-        });
-        break;
-      case 'marvin':
-        showSlider();
-        effectLevelSliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 100
-          },
-          start: 100,
-          step: 1
-        });
-        break;
-      case 'phobos':
-        showSlider();
-        effectLevelSliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 3
-          },
-          start: 3,
-          step: 0.1
-        });
-        break;
-      case 'heat':
-        showSlider();
-        effectLevelSliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 1,
-            max: 3
-          },
-          start: 3,
-          step: 0.1
-        });
-        break;
-      case 'none':
-        resetEffect(userPicture);
-        break;
-      default:
-        break;
+    currentFilter = evt.target.value;
+    userPicture.classList.add(`effects__preview--${currentFilter}`);
+    effectSliderContainer.classList.remove('hidden');
+    if (currentFilter === 'none') {
+      resetEffect(userPicture);
+    }
+    if (currentFilter) {
+      effectLevelSliderElement.noUiSlider.updateOptions({
+        range: {
+          min: effects[currentFilter].range.min,
+          max: effects[currentFilter].range.max
+        },
+        start: effects[currentFilter].start,
+        step: effects[currentFilter].step
+      });
     }
   });
 };
